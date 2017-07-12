fd4ed6508e9a0e7e15cdbe12183d5b1499b3a652
glenn mohre <glenn.mohre@gmail.com>
2017-07-12 13:58:25 -0400

    removal

--- drf_apis/settings.py
+++ drf_apis/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-from local_settings import AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
+from local_settings import AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, SECRET_KEY
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -21,7 +21,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%ixj6r*@dd^&e&1#e!+5@-^11qi1wsyk0i-db18-%rnmo+w%r#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
