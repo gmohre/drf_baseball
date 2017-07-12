772c66c451a3a1a4bc79289dcd7df834b09f7e4e
gmohre <glenn.mohre@gmail.com>
2017-07-12 17:03:57 -0400

    Update views.py

--- drf_apis/drf_baseball/views.py
+++ drf_apis/drf_baseball/views.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.shortcuts import render
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import viewsets
 from .models import NumpyFunction
 from .serializers import NumpyFunctionSerializer
 
-from django.shortcuts import render
 
 class NumpyFunctionViewSet(viewsets.ModelViewSet):
     queryset = NumpyFunction.objects.all()
