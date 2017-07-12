6b74ae2a186d92e754683ae3738df1a12624702c
glenn mohre <glenn.mohre@gmail.com>
2017-07-12 17:19:49 -0400

    including datastructures in main api

--- drf_apis/drf_baseball/serializers.py
+++ drf_apis/drf_baseball/serializers.py
@@ -1,12 +1,16 @@
 from rest_framework import serializers
 from .models import NumpyFunction, NumpyDataStructure
 
-class NumpyFunctionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = NumpyFunction
-        fields = ('__all__')
-
 class NumpyDataStructureSerializer(serializers.ModelSerializer):
     class Meta:
         model = NumpyDataStructure
         fields = ('__all__')
+
+class NumpyFunctionSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = NumpyFunction
+        fields = ('title', 'numpy_source', 'json', 'data')
+        depth = 1
+
+
